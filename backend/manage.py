067f25f14166c0e00bbf6f6d770bc93eabf76988
Martin JR <mmbenga0@gmail.com>
2026-01-24 21:16:10 +0100

    Force .env override

--- backend/manage.py
+++ backend/manage.py
@@ -5,8 +5,11 @@ import os
 import sys
 from typing import NoReturn
 
+from dotenv import load_dotenv
+
 
 def main() -> NoReturn:
+    load_dotenv(override=True)
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
     try:
         from django.core.management import execute_from_command_line
